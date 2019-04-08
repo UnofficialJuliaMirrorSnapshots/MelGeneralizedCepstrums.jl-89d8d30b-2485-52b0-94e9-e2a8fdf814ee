987ac2b16e32d206c934b139d33f7f8d790bd592
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 00:10:43 +0000

    Snapshot of branch master taken on 2019-04-07T20:10:42.487-04:00 from "https://github.com/UnofficialJuliaMirror/MelGeneralizedCepstrums.jl-89d8d30b-2485-52b0-94e9-e2a8fdf814ee"

---
+++ README.md
@@ -0,0 +1,44 @@
+# MelGeneralizedCepstrums
+
+[![MelGeneralizedCepstrums](http://pkg.julialang.org/badges/MelGeneralizedCepstrums_0.4.svg)](http://pkg.julialang.org/?pkg=MelGeneralizedCepstrums&ver=0.4)
+[![MelGeneralizedCepstrums](http://pkg.julialang.org/badges/MelGeneralizedCepstrums_0.5.svg)](http://pkg.julialang.org/?pkg=MelGeneralizedCepstrums&ver=0.5)
+[![MelGeneralizedCepstrums](http://pkg.julialang.org/badges/MelGeneralizedCepstrums_0.6.svg)](http://pkg.julialang.org/?pkg=MelGeneralizedCepstrums&ver=0.6)
+
+[![Build Status](https://travis-ci.org/r9y9/MelGeneralizedCepstrums.jl.svg?branch=master)](https://travis-ci.org/r9y9/MelGeneralizedCepstrums.jl)
+[![Build status](https://ci.appveyor.com/api/projects/status/gr17ty0m7fagqsh5/branch/master?svg=true)](https://ci.appveyor.com/project/r9y9/melgeneralizedcepstrums-jl/branch/master)
+[![Coverage Status](https://coveralls.io/repos/r9y9/MelGeneralizedCepstrums.jl/badge.svg?branch=master)](https://coveralls.io/r/r9y9/MelGeneralizedCepstrums.jl?branch=master)
+[![License](http://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat)](LICENSE.md)
+
+![](examples/mel-generalized-cepstrum.png)
+
+MelGeneralizedCepstrums.jl provides a mel generalized cepstrum anlysis for spectral envelope estimation that includes:
+
+- linear predicition analysis (LPC)
+- generalized cepstrum analysis
+- mel-cepstrum analysis
+- mel-generalized cepstrum analysis
+
+The package also provides conversions, e.g, mel-generalized cepstrum to mel-cepstrum, mel-cepstrum to (linear frequency) cepstrum, mel-cesptrum to filter coefficients, and vise versa.
+
+Note that this package is built on top of [SPTK.jl](https://github.com/r9y9/SPTK.jl). The package is designed to provide a Julia-like interface with a focus on the mel-generalized cepstrum analysis. A part of the core is re-writen in Julia from [Speech Signal Processing Toolkit (SPTK)](http://sp-tk.sourceforge.net/).
+
+## Demonstration notebook
+
+- [Introduction notebook](http://nbviewer.ipython.org/github/r9y9/MelGeneralizedCepstrums.jl/blob/master/examples/Introduction%20to%20MelGeneralizedCeptrums.jl.ipynb): a brief introduction that shows how the mel-generalized cepstrum analysis works.
+
+## Supported Platforms
+
+- Linux
+- Mac OS X
+- Windows
+
+## Installation
+
+```julia
+Pkg.add("MelGeneralizedCepstrums")
+```
+
+## Related packages
+
+- [SynthesisFilters.jl](https://github.com/r9y9/SynthesisFilters.jl): speech waveform synthesis filters, especially from mel-generalized cepstrums
+- [WORLD.jl](https://github.com/r9y9/WORLD.jl): a lightweight julia wrapper for WORLD - a high-quality speech analysis, modification and synthesis system
---
+++ test/sptk.jl
@@ -0,0 +1,239 @@
+# Check consistency with SPTK
+
+module SPTKTest
+
+using MelGeneralizedCepstrums
+using Base.Test
+
+import SPTK
+import MelGeneralizedCepstrums: lpc2c!, b2c, frqtr, frqtr!
+
+function test_mcep(order::Int, α::Float64)
+    srand(98765)
+    x = rand(512)
+    mc = SPTK.mcep(x, order, α)
+    mĉ = MelGeneralizedCepstrums._mcep(x, order, α)
+    @test mc ≈ mĉ
+end
+
+function test_mgcep(order::Int, α::Float64, γ::Float64)
+    srand(98765)
+    x = rand(512)
+    mgc = SPTK.mgcep(x, order, α, γ; threshold=0.001)
+    mgĉ = MelGeneralizedCepstrums._mgcep(x, order, α, γ; criteria=0.001)
+    @test mgc ≈ mgĉ
+end
+
+function test_lpc2c()
+    srand(98765)
+    a = rand(21)
+
+    c = SPTK.lpc2c(a)
+    ĉ = lpc2c(a)
+    @test c ≈ ĉ
+    ĉ = similar(a)
+    lpc2c!(ĉ, a)
+    @test c ≈ ĉ
+end
+
+function test_gnorm(γ::Float64)
+    srand(98765)
+    mc = rand(21)
+
+    g = SPTK.gnorm(mc, γ)
+    ĝ = gnorm(mc, γ)
+    @test g ≈ ĝ
+    ĝ = copy(mc)
+    gnorm!(ĝ, γ)
+    @test g ≈ ĝ
+end
+
+function test_ignorm(γ::Float64)
+    srand(98765)
+    mc = rand(21)
+
+    g = SPTK.ignorm(mc, γ)
+    ĝ = ignorm(mc, γ)
+    @test g ≈ ĝ
+    ĝ = copy(mc)
+    ignorm!(ĝ, γ)
+    @test g ≈ ĝ
+end
+
+function test_mc2b(α::Float64)
+    srand(98765)
+    mc = rand(21)
+
+    b = SPTK.mc2b(mc, α)
+    b̂ = mc2b(mc, α)
+    @test b ≈ b̂
+    b̂ = copy(mc)
+    mc2b!(b̂, α)
+    @test b ≈ b̂
+end
+
+
+function test_b2mc(α::Float64)
+    srand(98765)
+    b = rand(21)
+
+    mc = SPTK.b2mc(b, α)
+    mĉ = b2mc(b, α)
+    @test mc ≈ mĉ
+end
+
+function test_c2ir(len::Int=512)
+    srand(98765)
+    c = rand(21)
+
+    ir = SPTK.c2ir(c, len)
+    ir̂ = c2ir(c, len)
+    @test ir ≈ ir̂
+end
+
+function test_freqt(order::Int, α::Float64)
+    srand(98765)
+    mc = rand(21)
+
+    m = SPTK.freqt(mc, order, α)
+    m̂ = freqt(mc, order, α)
+    @test m ≈ m̂
+end
+
+function test_b2c(order::Int, α::Float64)
+    srand(98765)
+    mc = rand(21)
+
+    m = SPTK.b2c(mc, order, α)
+    m̂2 = b2c(mc, order, α)
+    @test m ≈ m̂2
+end
+
+function test_frqtr(order::Int, α::Float64)
+    srand(98765)
+    mc = rand(21)
+
+    m = SPTK.frqtr(mc, order, α)
+    m̂ = frqtr(mc, order, α)
+    @test m ≈ m̂
+end
+
+function test_gc2gc(order::Int, γ::Float64)
+    srand(98765)
+    gc = rand(21)
+
+    gc2 = SPTK.gc2gc(gc, 0.0, order, γ)
+    gc2̂ = gc2gc(gc, 0.0, order, γ)
+    @test gc2 ≈ gc2̂
+end
+
+function test_mgc2mgc(order::Int, α::Float64, γ::Float64)
+    srand(98765)
+    mgc = rand(21)
+
+    mgc2 = SPTK.mgc2mgc(mgc, 0.41, 0.0, order, α, γ)
+    mgc2̂ = mgc2mgc(mgc, 0.41, 0.0, order, α, γ)
+    @test mgc2 ≈ mgc2̂
+end
+
+function test_mgc2sp(α::Float64, γ::Float64)
+    srand(98765)
+    mgc = rand(21)
+
+    fftlen = 512
+    sp = SPTK.mgc2sp(mgc, α, γ, fftlen)
+    sp̂ = mgc2sp(mgc, α, γ, fftlen)
+    @test sp ≈ sp̂
+    @test length(sp̂) == fftlen>>1 + 1
+    @test eltype(sp̂) == Complex{eltype(mgc)}
+end
+
+for order in 10:2:30
+    for α in [-0.544, -0.41, -0.35, 0.0, 0.35, 0.41, 0.544]
+        println("mcep: testing with order=$order, α=$α")
+        test_mcep(order, α)
+    end
+end
+
+for order in 25:5:35
+    for α in [-0.544, -0.41, -0.35, 0.0, 0.35, 0.41, 0.544]
+        for γ in [-1.0, -0.75, -0.5, -0.25, 0.0]
+            println("mgcep: testing with order=$order, α=$α, γ=$γ")
+            test_mgcep(order, α, γ)
+        end
+    end
+end
+
+for γ in [-1.0, -0.75, -0.5, -0.25, 0.0]
+    println("gnorm: testing with γ=$γ")
+    test_gnorm(γ)
+end
+
+for γ in [-1.0, -0.75, -0.5, -0.25, 0.0]
+    println("ignorm: testing with γ=$γ")
+    test_ignorm(γ)
+end
+
+for α in [-0.544, -0.41, -0.35, 0.0, 0.35, 0.41, 0.544]
+    println("mc2b: testing with α=$α")
+    test_mc2b(α)
+end
+
+for α in [-0.544, -0.41, -0.35, 0.0, 0.35, 0.41, 0.544]
+    println("mc2b: testing with α=$α")
+    test_b2mc(α)
+end
+
+for len in [128, 256, 512, 1024]
+    println("c2ir: testing with len=$len")
+    test_c2ir(len)
+end
+
+for order in 10:2:30
+    for α in [-0.544, -0.41, -0.35, 0.0, 0.35, 0.41, 0.544]
+        println("freqt: testing with order=$order, α=$α")
+        test_freqt(order, α)
+    end
+end
+
+for order in 10:2:30
+    for α in [-0.544, -0.41, -0.35, 0.0, 0.35, 0.41, 0.544]
+        println("b2c: testing with order=$order, α=$α")
+        test_b2c(order, α)
+    end
+end
+
+for order in 10:2:30
+    for α in [-0.544, -0.41, -0.35, 0.0, 0.35, 0.41, 0.544]
+        println("frqtr: testing with order=$order, α=$α")
+        test_frqtr(order, α)
+    end
+end
+
+for order in 15:5:35
+    for γ in [-1.0, -0.75, -0.5, -0.25, 0.0]
+        println("gc2gc: testing with order=$order, γ=$γ")
+        test_gc2gc(order, γ)
+    end
+end
+
+for order in 15:5:35
+    for α in [-0.544, -0.41, -0.35, 0.35, 0.41, 0.544]
+        for γ in [-1.0, -0.75, -0.5, -0.25, 0.0]
+            println("mgc2mgc: testing with order=$order, α=$α, γ=$γ")
+            test_mgc2mgc(order, α, γ)
+        end
+    end
+end
+
+for α in [-0.544, -0.41, -0.35, 0.35, 0.41, 0.544]
+    for γ in [-0.75, -0.5, -0.25, 0.0] # TODO(ryuichi) -1.0
+        println("mgc2sp: testing with α=$α and γ=$γ")
+        test_mgc2sp(α, γ)
+    end
+end
+
+println("lpc2c: testing")
+test_lpc2c()
+
+end # module SPTKTestModule
